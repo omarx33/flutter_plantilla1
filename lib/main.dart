175c84e13f65ff1ac72055fb46ee46928aaa058d
omarx33 <carlosmorih33@gmail.com>
2023-07-17 05:51:35 -0500

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.white70,
+              backgroundImage: AssetImage("assets/img/persona1.jpg"),
+              // backgroundImage: NetworkImage(
+              //     "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+            ),
+            SizedBox(height: 12.0,),
+            Text(
+              "Ana estrada",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w700,
+                  fontFamily: "Pangolin",
+
+              ),
+            ),
+            SizedBox(height: 4.0,),
+            Text(
+              "Flutter programador",
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 15.0,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400
+              ),
+            ),
+            SizedBox(
+              width: 170.0,
+              child: Divider(
+                thickness: 0.8,
+                color: Colors.white,
+                indent: 20.0,
+                endIndent: 20.0,
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
+              elevation: 10.0,//sonbreado inferior
+              child: ListTile(
+                leading: Icon(Icons.phone,
+                color: Colors.indigo,), //izquierda
+                title: Text("+51 22222222"),
+                subtitle: Text("Teléfono"),
+                trailing: Icon(Icons.check_circle_outline), //derecha
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 14.0, vertical: 8.0),
+              elevation: 10.0,//sonbreado inferior
+              child: ListTile(
+                leading: Icon(Icons.mail,
+                  color: Colors.indigo,), //izquierda
+                title: Text("prueba@gmail.com"),
+                subtitle: Text("correo electrónico"),
+                trailing: Icon(Icons.check_circle_outline), //derecha
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Image.asset(
+                "assets/img/facebook.png",
+                height: 60.0,
+              ),
+                SizedBox(
+                  width: 30.0,
+                ),
+              Image.asset(
+                "assets/img/twitter.png",
+                height: 60.0,
+              ),
+                SizedBox(
+                  width: 30.0,
+                ),
+              Image.asset(
+                "assets/img/instagram.png",
+                height: 60.0,
+              ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
